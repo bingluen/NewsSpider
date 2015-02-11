ddfaac205101a44fdc6d881c6ca2e4c5f9d4e0fd
erickson-makotoki <a820224@gmail.com>
2015-02-11 23:05:28 +0800

    add loop betwen two day

--- newsSpider.py
+++ newsSpider.py
@@ -1,21 +1,34 @@
 import sys
 import chinatimes
 import ltn
+import datetime
+
+startDay = datetime.date(int(sys.argv[2][0:4]), int(sys.argv[2][4:6]), int(sys.argv[2][6:8]))
+endDay = datetime.date(int(sys.argv[3][0:4]), int(sys.argv[3][4:6]), int(sys.argv[3][6:8]))
 
 if sys.argv[1] == 'chinatimes':
 	chinatime_spider = chinatimes.NewsSpider('chinatimes', 'chinatimes')
-	chinatime_spider.setDate(sys.argv[2])
-	chinatime_spider.execute()
+	currentDay = startDay
+	while currentDay < endDay:
+		chinatime_spider.setDate(str(currentDay))
+		chinatime_spider.execute()
+		currentDay += datetime.timedelta(days=1)
 
 if sys.argv[1] == 'ltn':
 	s = ltn.NewsSpider('ltn')
-	s.setDate(sys.argv[2])
-	s.execute()
+	currentDay = startDay
+	while currentDay < endDay:
+		s.setDate(str(currentDay))
+		s.execute()
+		currentDay += datetime.timedelta(days=1)
 
 if sys.argv[1] == '--all':
 	chinatime_spider = chinatimes.NewsSpider('chinatimes', 'chinatimes')
-	chinatime_spider.setDate(sys.argv[2])
-	chinatime_spider.execute()
 	s = ltn.NewsSpider('ltn')
-	s.setDate(sys.argv[2])
-	s.execute()
+	currentDay = startDay
+	while currentDay < endDay:
+		chinatime_spider.setDate(str(currentDay))
+		chinatime_spider.execute()
+		s.setDate(str(currentDay))
+		s.execute()
+		currentDay += datetime.timedelta(days=1)
